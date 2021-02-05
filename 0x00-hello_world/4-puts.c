ea39d7f6890818bcbdebf446088deb64ca36e438
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-02-05 17:56:56 +0000

    4-puts.c file Updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-/*
- * main - main block
- * 
+/**
+ * main - code block
+ * Description: To print a string
  * Return: 0
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
