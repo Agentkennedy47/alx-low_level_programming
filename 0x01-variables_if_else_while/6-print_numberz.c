bba1b03c2a5edf1854459c28c464350d218e4a6f
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-02-05 21:31:54 +0000

    FILE MODIFIED

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,6 +2,9 @@
 /**
  * main - Code block
  * Description: Print single digit numbers of base 10
+ * You are not allowed to use any variable of type `char`
+ * You can only use `putchar` to print to console
+ * You can only use `putchar` twice
  * Return: 0
  */
 int main(void)
@@ -9,7 +12,7 @@ int main(void)
 
 	int i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 
 		putchar(i + '0');
