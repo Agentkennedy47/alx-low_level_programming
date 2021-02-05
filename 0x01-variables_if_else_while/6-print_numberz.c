aa4affd4d94ed9c2d6397c06932ab58963d51cf8
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-02-05 01:35:39 +0000

    6-print_numberz.c file created

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Code block
+ * Description: Print single digit numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+
+		putchar(i + '0');
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
