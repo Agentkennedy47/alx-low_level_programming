d8e1ac5426b6053cca52b1050f964ec6d7ba7cef
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-02-05 01:14:32 +0000

    5-print_numbers.c file created

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - code block
+ * Description: To print single digit numbers of
+ * base 10 including 0
+ * Return: 0
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+
+		printf("%i", i);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
