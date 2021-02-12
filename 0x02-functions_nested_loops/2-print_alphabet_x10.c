58120386d4335950438ac6923166be0c6517e06d
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-02-12 14:50:58 +0000

    2-print_alphabet_x10.c file created

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - code entry point
+ * Description: To print the alphabet 10 times and in lowercase
+ * Return: 0 always successful
+ */
+void print_alphabet_x10(void)
+{
+
+	char a, b;
+
+	for (a = 0; a <= 10; a++)
+	{
+
+	       while (j <= 'z')
+	       {
+
+		       _putchar(j);
+		       j++;
+	       }
+	}
+
+	_putchar ('\n');
+
+}
