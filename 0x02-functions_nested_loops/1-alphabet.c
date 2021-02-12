b43b17f28524f7b7420e70b0350f997275a202e1
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-02-12 11:39:57 +0000

    1-alphabet.c file created

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * main - code entry point
+ *
+ * Return: 0 is always a success
+ */
+void print_alphabet(void)
+{
+
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+
+		_putchar(x);
+		x++;
+	}
+
+	_putchar('\n');
+
+}
