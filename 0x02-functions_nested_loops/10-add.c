1d3a6cf81018fc48249e1a049fe65499161d1aa3
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-02-16 00:42:39 +0000

    10-add.c file created

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * add - code block entry point
+ * Description: To add two integers
+ *
+ * Return: 0 is successful
+ */
+int add(int, int)
+{
+
+	int i;
+	int j;
+	int sum;
+
+	sum = (i + j);
+
+	_putchar(sum + '0');
+
+}
