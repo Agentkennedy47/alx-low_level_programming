1c6237a9d112d32e2a339808364a2ca6f6f472fe
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-02-22 00:20:58 +0000

    2-mul.c file created

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * mul - code entry point
+ * Description: Function that multiplies two integers
+ * @a: number to be multiplied
+ * @b: number to be multiplied
+ * Return: multiplied values
+ */
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+}
