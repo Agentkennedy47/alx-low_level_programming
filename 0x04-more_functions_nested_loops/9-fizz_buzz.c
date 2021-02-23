85ad12506d5376d6758c8d83bdfda650643e4742
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-02-23 23:10:36 +0000

    File updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,7 +19,7 @@ int main(void)
 			printf("FizzBuzz");
 		else if ((num % 3) == 0)
 			printf("Fizz");
-		else if((num % 5) == 0)
+		else if ((num % 5) == 0)
 			printf("Buzz");
 		else
 			printf("%d", num);
