6014be5c93d586e0f63a51b8acc8511d6a29b9fd
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-02-23 16:40:57 +0000

    6-print_line.c file created

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_line - code entry point
+ * Description: Function that draws a straight line
+ * @n: int type number
+ * Return: 0 is a success
+ */
+void print_line(int n)
+{
+
+	int x;
+
+	while (x < n)
+	{
+
+		_putchar('_');
+
+	}
+
+	_putchar('\n');
+
+}
