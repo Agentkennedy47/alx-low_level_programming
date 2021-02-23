d5d7682bd9d7c74b4a30d51c8659a2cc4a34e6d0
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-02-23 23:03:07 +0000

    9-fizz_buzz.c file created

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - code entry point
+ * Description: A program that prints from 1 to 100
+ * Return: 0 is a success
+ */
+int main(void)
+{
+
+
+	int num = 1;
+
+	while (num <= 100)
+	{
+
+		if (num != 1)
+			printf(" ");
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
+		else if ((num % 3) == 0)
+			printf("Fizz");
+		else if((num % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
+		num++;
+	}
+
+	putchar('\n');
+	return (0);
+
+}
