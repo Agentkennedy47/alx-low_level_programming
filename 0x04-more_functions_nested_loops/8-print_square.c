0c0dbfa2c6ededf071e833b823a12d85a6055cfd
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-02-23 20:03:10 +0000

    8-print_square.c file created

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+/**
+ * print_square - code entry point
+ * Description: Function that prints a square
+ * @size: int type
+ * You can only use `_putchar` function to print
+ * Return: 0
+ */
+void print_square(int size)
+{
+
+
+	int x, y, z;
+
+	x = 0;
+
+	if (x >= size)
+	{
+
+		_putchar('\n');
+	}
+	else
+	{
+
+		while (x < size)
+		{
+
+			for(y = 0; y < size; y++)
+			{
+
+				_putchar('#');
+
+			}
+			for (z = 0; z < size; z++)
+			{
+
+				_putchar('#');
+
+			}
+			_putchar('\n');
+			x++;
+
+		}
+
+	}
+
+
+}
