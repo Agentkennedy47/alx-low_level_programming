76be49e8d043c0d99fa1499ea8854d0a69e1f4ff
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-02-23 17:21:12 +0000

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_diagonal - code entry point
+ * Description: Function that draws a diagonal line
+ * @n: int type number
+ * You can only use `_putchar` function to print
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+
+
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+
+		_putchar("'\'");
+
+	}
+
+	_putchar('\n');
+
+
+}
