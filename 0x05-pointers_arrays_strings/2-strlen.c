20f893e159e5b381e2e996cb722a3d4854101566
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-01 14:40:48 +0000

    2-strlen.c file created

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen - code entry point
+ * Description: Function that returns the length of a string
+ * @s: string type char
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+
+
+	return (strlen(*s));
+
+
+}
