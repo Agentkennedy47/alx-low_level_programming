787d5a140a68a113d2034824c22466f4539a7f99
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-01 14:49:14 +0000

    2-strlen.c file updated

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,22 @@
 /**
  * _strlen - code entry point
  * Description: Function that returns the length of a string
- * @s: string type char
- * Return: 0
+ * @s: string type
+ * Return: Length of a string
  */
 int _strlen(char *s)
 {
 
 
-	return (strlen(*s));
+	int size = 0;
 
+	while (*s != '\0')
+	{
+
+		size++;
+		*s++;
+	}
+
+	return (size);
 
 }
