917616eafe11ffef820b67c3316d5cb7aff8ca01
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-01 18:13:24 +0000

    3-puts.c file modified

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,15 +8,15 @@
 void _puts(char *str)
 {
 
+	int i = 0;
 
-	while (*str != '\0')
+	while (*(str + i) != '\0')
 	{
 
-		_putchar(*str);
-		str++;
+		_putchar(*(str + i));
+		i++;
 
 	}
 
 	_putchar('\n');
-
 }
