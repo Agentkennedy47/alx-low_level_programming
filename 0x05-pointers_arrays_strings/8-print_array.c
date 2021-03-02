6f1ec81c979c6a0a582d20b2861216bffd57f2d6
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-02 19:07:32 +0000

    8-print_array.c file created

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_array - code entry point
+ * Description: Function that prints `n` elements of an array of integers
+ * @a: int type
+ * @n: int type
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+
+
+	
+
+
+}
