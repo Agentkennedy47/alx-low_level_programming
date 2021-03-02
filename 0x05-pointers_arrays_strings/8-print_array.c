4af71a224621cfcdd1f2920748a9bedc86b78648
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-02 19:35:20 +0000

    File modified

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * print_array - code entry point
  * Description: Function that prints `n` elements of an array of integers
@@ -10,7 +11,19 @@ void print_array(int *a, int n)
 {
 
 
-	
+	int i;
 
+	for (i = 0; i < n; i++)
+	{
+
+
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+			printf(", ");
+
+	}
+
+	printf("\n");
 
 }
