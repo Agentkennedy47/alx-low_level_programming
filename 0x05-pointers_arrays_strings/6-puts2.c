f6e63ad4ea289a5bde44733e03c0f2ff87de3027
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-02 15:49:24 +0000

    6-puts2.c file created

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * puts2 - code entry point
+ * Description: Function that prints every other character of a string
+ * starting with the first character
+ * @str: string type
+ * Return: 0
+ */
+void puts2(char *str)
+{
+
+
+	
+
+
+}
