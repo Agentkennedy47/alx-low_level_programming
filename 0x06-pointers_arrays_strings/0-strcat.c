494bd4e17828251763a5762431fef28691ca896e
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-04 21:01:52 +0000

    0-strcat.c file created

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * *_strcat - code entry point
+ * Description: Function that concatenates two string
+ * @src: char type
+ * @dest: char type
+ * Return: Pointer to the string `dest`
+ */
+char *_strcat(char *dest, char *src)
+{
+
+
+	int length; j;
+
+	length = 0;
+
+	while (src[length] != '\0')
+	{
+
+		length++;
+
+	}
+
+	for (j = 0; 
+
+
+}
