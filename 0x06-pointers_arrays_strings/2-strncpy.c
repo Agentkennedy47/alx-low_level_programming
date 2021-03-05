a55b64cff8ccfd3887cd62fd2ae038a1f7a4c30f
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-05 22:25:54 +0000

    2-strncpy.c file created

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * *_strncpy - code entry point
+ * Description: Function that copies a string
+ * @dest: sting type
+ * @src: string type
+ * @n: number of byte to append
+ * Return: 0 always successful
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int length = 0, j = 0;
+
+	while (src[length] != '\0')
+		j++;
+	for (length = 0; src[length] && length < n; length++)
+		dest[length] = src[length];
+	for (length = j; length < n; length++)
+		dest[length] = '\0';
+
+	return (dest);
+
+
+}
