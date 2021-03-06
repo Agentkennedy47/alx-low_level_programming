5725c2aaf223b121105aca4d700df1bc00b5b6e0
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-06 22:57:45 +0000

    4-rev_array.c file created

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * reverse_array - code entry point
+ * Description: Function that reverses the content of an array
+ * @a: int type number
+ * @n: number of elements of the array
+ * Return: 0
+ */
+void reverse_array(int *a, int n)
+{
+
+
+	int i, j;
+
+	for (i = 0; i < n/2; i++)
+	{
+
+		j = a[i];
+		a[i] = a[n - i -1];
+		a[n - i - 1] = j;
+
+	}
+
+
+}
