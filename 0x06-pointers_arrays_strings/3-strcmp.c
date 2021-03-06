c1dd5bfa9bdfb3d6b5efb8c7b534f79b172078ff
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-06 00:30:27 +0000

    3-strcmp.c file updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,20 +10,15 @@
 int _strcmp(char *s1, char *s2)
 {
 
-	while (*s1 == *s2)
+	int i = 0, j = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0' && j == 0)
 	{
-
-		if (*s1 == '\0' || *s2 == '\0')
-			break;
-
-		s1++;
-		s2++;
+		j = s1[i] - s2[i];
+		i++;
 
 	}
 
-	if (*s1 == '\0' && *s2 == '\0')
-		return (0);
-	else
-		return (-1);
+	return (j);
 
 }
