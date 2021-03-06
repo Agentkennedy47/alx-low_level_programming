9cbc3740b062cdf539c5070b3a708034add2e745
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-06 23:10:58 +0000

    4-rev_array.c file modified

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,11 +12,11 @@ void reverse_array(int *a, int n)
 
 	int i, j;
 
-	for (i = 0; i < n/2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 
 		j = a[i];
-		a[i] = a[n - i -1];
+		a[i] = a[n - i - 1];
 		a[n - i - 1] = j;
 
 	}
