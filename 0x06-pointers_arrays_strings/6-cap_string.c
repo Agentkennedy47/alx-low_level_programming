d4305b0355b01354ec0d645e18b7c2478ffec652
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-07 13:16:37 +0000

    6-cap_string.c file created

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+/**
+ * *cap_string - code point entry
+ * Description: Function that capitalizes all words of a string
+ * @str: string type
+ * Return: points to string
+ */
+char *cap_string(char *str)
