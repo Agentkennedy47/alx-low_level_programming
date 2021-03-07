5c756251b1281f8a67263a2bd7283d0fb6a9bf88
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-07 21:02:30 +0000

    100-print_number.c file created

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * print_number - code block
+ * Description: Fucntion that prints an integer
+ * @n: int type number
+ * You can only use `_putchar` function
+ * Return: 0
+ */
+void print_number(int n)
+{
+
+	char c = '0';
+
+	if (n < 0)
+	{
+
+		if (n % 10 == 0)
+		{
+
+			_putchar('-');
+			n = -n;
+		}
+		else
+		{
+
+			_putchar('-');
+			n++;
+			c = '1';
+			n = -n;
+		}
+
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + c);
+
+}
