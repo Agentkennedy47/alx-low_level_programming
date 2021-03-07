a82564d28b63403b8a46bf98ca6b063d6b0c07ea
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-07 18:47:14 +0000

    8-rot13.c file created

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#inlcude "holberton.h"
+/**
+ * *rot13 - code entry point
+ * Description: Function that encodes a string using `rot13`
+ * @str: string type
+ * Return: Points to string
+ */
+char *rot13(char *str)
+{
+	int i = 0, j;
+
+	char arr[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIHKLMNOPQRSTUVWXYZ";
+	char arr2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; j <= 52; j++)
+		{
+			if (str[i] == arr[j])
+			{
+				str[i] = arr2[j];
+				break;
+			}
+		}
+
+		i++;
+	}
+
+	return (str);
+}
