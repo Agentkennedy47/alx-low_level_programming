ab9cb4a50b427956a785a51d9d9d40746e5351b8
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-08 20:04:14 +0000

    4-strpbrk.c file updated

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,3 +3,27 @@
  * *_strpbrk - Code block point
  * Description: Function that searches a string for any set of bytes
  * @s: First occurrence in the string
+ * @accept: The set of bytes to be searched
+ *
+ * Return: Points to the byte in @s that matches one of the
+ * bytes in @accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int index;
+
+	while (*s)
+	{
+
+		for (index = 0; accept[index]; index++)
+		{
+
+			if (*s == accept[index])
+				return (s);
+		}
+
+		s++;
+	}
+
+	return ('\0');
+}
