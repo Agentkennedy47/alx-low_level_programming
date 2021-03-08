344f20e385277d55a5d73a6422f72fcf5d2764d3
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-08 16:37:27 +0000

    3-strspn.c file created

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * _strspn - Code block
+ * Description: Function that gets the length of a prefix substring
+ * @s: String to be searched for
+ * @accept: Prefix to be measured
+ *
+ * Return: The number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+
+	unsigned int bytes = 0;
+	int x;
+
+	while (*s)
+	{
+
+		for (x = 0; accept[x]; x++)
+		{
+
+			if (*s == accept[x])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[x] == '\0')
+				return (bytes);
+		}
+
+		s++;
+
+	}
+
+	return (bytes);
+
+
+}
