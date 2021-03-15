37ac0425177dc01ebc746f6fc04be046adab3265
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-15 21:25:31 +0000

    0-puts_recursion.c file created

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - Code block entry
+ * Description: Function that prints a string
+ * @s: String to be printed
+ *
+ *
+ */
+void _puts_recursion(char *s)
+{
+
+	if (*s)
+	{
+
+		_putchar(*s);
+		_puts_recursion(s++);
+	}
+
+	_putchar('\n');
+
+}
