88eefeaf2de431761c592ee22d3d174699f7b609
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-15 23:56:34 +0000

    3-factorial.c file created

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * factorial - Code block entry
+ * Description: Function that returns the factorial of a given number
+ * @n: Number to be measured
+ * Return: -1 to indicate error if `n` is lower than 0
+ */
+int factorial(int n)
+{
+
+        if (n < 0)
+                return (-1);
+        else if (n == 0)
+                return (1);
+
+        return (n * factorial(n - 1));
+
+}
