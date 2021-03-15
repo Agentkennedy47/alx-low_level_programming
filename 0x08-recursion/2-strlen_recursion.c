3effa6d3792396d6633ca9e902a2b244b7bbb036
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-15 23:01:32 +0000

    2-strlen_recursion.c file created

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - Code block entry
+ * Description: Function that returns the length of a string
+ * @s: String to be printed
+ *
+ */
+int _strlen_recursion(char *s)
+{
+
+	int length = 0;
+	int i;
+
+	if (i = 0; i <= length; i++)
+	{
+
+		_strlen_recursion(length++);
+		_putchar(*s);
+	}
+
+}
