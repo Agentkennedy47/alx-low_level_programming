b946be36ec4956fdd4d1e777029fa2464f97d8e2
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-16 00:02:38 +0000

    3-factorial.c file modified

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,11 +8,17 @@
 int factorial(int n)
 {
 
-        if (n < 0)
-                return (-1);
-        else if (n == 0)
-                return (1);
+	if (n < 0)
+	{
 
-        return (n * factorial(n - 1));
+		return (-1);
+	}
+	else if (n == 0)
+	{
+
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
 
 }
