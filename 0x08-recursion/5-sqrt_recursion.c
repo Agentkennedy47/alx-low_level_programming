649e98bd6e889cb97b9a3772ffd3f50963dc0405
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-17 19:32:24 +0000

    5-sqrt_recursion.c file updated

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,44 @@
 #include "holberton.h"
 /**
- * _sqrt_recursion - Code block
- * Description: Function that returns the natural square root
- * @n: Number to be measured
- * Return: Function return -1, if `n` is not a natural square root
+ * _sqrt_root - Code block
+ * Description: Find the square root
+ * @n: int type
+ * @i: int type
+ * Return: -1 if i*i > n else return sqrt(n, i+1), n if i*i = n
  */
+int _sqrt_root(int n, int i)
+{
+
+	if (i * i == n)
+	{
+
+		return (i);
+	}
+	else if (i * i > n)
+	{
+
+		return (-1);
+	}
+
+	return (_sqrt_root(n, i + 1));
+
+}
+
+/**
+ * _sqrt_recursion - Code block
+ * Description: Find the square root of n
+ * @n: int type
+ * Return: sqrt(n,0)
+ */
+int _sqrt_recursion(int n)
+{
+
+	if (n < 1)
+	{
+
+		return (-1);
+	}
+
+	return (_sqrt_root(n, 0));
+
+}
