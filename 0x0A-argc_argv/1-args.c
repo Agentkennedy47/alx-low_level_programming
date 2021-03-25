9c83e7aa798d50a56882ce2f4f7cc10bcbc3062f
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-25 20:03:36 +0000

    1-args.c file created

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Code block entry
+ * Description: Program that prints the number of arguments
+ * @argc: int type
+ * @argv: char type
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+	printf("%d\n", argc);
+	return (0);
+
+}
