f3916fa649a9be30e11ef8c42f1bd0b605da0da3
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-25 20:37:00 +0000

    2-args.c file created

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Code block entry
+ * Description: Program that prints all arguments
+ * @argc: int type
+ * @argv: char type
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+	int i;
+
+	if (i = 0; i < argc; i++)
+	{
+
+		printf("%s\n", i, argv[i]);
+
+	}
+
+	return (0);
+
+}
