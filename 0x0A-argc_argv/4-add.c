f5e4934f2eab49db624def2d306b63edd382c116
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-25 22:16:59 +0000

    4-add.c file modified

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,15 +10,23 @@
 int main(int argc, char *argv[])
 {
 
-	int x;
-	int y;
+	int i;
 	int sum = 0;
 
-	if (argc != 2)
+	for (i = 1; i < argc; i++)
 	{
 
+		if (!atoi(argv[i]))
+		{
 
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += atoi(argv[i]);
 
 	}
 
+	printf("%d\n", sum);
+	return (0);
 }
