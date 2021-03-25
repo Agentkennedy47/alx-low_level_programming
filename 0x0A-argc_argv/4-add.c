d7b15a7d3ded9c2f43270dd20435a3ed49757734
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-25 21:44:01 +0000

    4-add.c file created

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Code entry block
+ * Description: Program that adds positive numbers
+ * @argc: int type
+ * @argv: char type
+ * Return: Print error if digits contain symbols, return 1
+ */
+int main(int argc, char *argv[])
+{
+
+	int x;
+	int y;
+	int sum = 0;
+
+	if (argc != 2)
+	{
+
+
+
+	}
+
+}
