3cc13482eef790290c37a23dd3e8fc3c8530f4c5
Agentkennedy47 <ofoeziekennedy91@gmail.com>
2021-03-25 19:53:55 +0000

    0-whatsmyname.c file created

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Code block entry
+ * Description: Program that prints its name
+ * @argc: int type
+ * @argv: char type
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
